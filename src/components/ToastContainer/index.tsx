d55c599bf66902c5709b1bcc36ccbd642cdff668
Marcelo de Lima Veraldo <marcelolimav@gmail.com>
2020-06-21 16:15:03 -0300

    fix: fim da implementação da tela de login e cadastro.

---
+++ src/components/ToastContainer/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useTransition } from 'react-spring';
+
+import Toast from './Toast';
+
+import { TaostMessage } from '../../hooks/toast';
+import { Container } from './styles';
+
+interface ToastContainerProps {
+  messages: TaostMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransictions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0, transform: 'rotateZ(0deg)' },
+      enter: { right: '0%', opacity: 1, transform: 'rotateZ(360deg)' },
+      leave: { right: '-120%', opacity: 0, transform: 'rotateZ(0deg)' },
+    },
+  );
+  return (
+    <Container>
+      {messagesWithTransictions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
+      ))}
+    </Container>
+  );
+};
+
+export default ToastContainer;
